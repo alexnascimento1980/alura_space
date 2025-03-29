c5fbbf6779aff575526ec4676801b244b35db1b1
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-03-28 21:27:46 -0300

    static

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1><h2>Alex Nascimento</h2>')
+    return render(request, 'galeria/index.html')
 
