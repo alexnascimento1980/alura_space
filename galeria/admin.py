fd0718e48a6ff84323df9000a57b0e01ad82d8bd
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-01 22:11:55 -0300

    configurando Admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class LIstandoFotografia(admin.ModelAdmin):
+    list_display = ('id','nome', 'legenda', 'foto')
+    list_display_links = ('id','nome')
+    search_fields = ('nome',)
+
+admin.site.register(Fotografia, LIstandoFotografia)
