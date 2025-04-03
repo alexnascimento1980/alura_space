4b8c50c201b220a908e2b53c9dcf26d7b5c60d6a
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-03 15:32:28 -0300

    inserir formulario

---
+++ usuarios/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login(request):
+    pass
+    return render(request, 'usuarios/login.html')
+
+def cadastro(request):
+    pass
+    return render(request, 'usuarios/cadastro.html')
